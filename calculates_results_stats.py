69c342df2198bb4ff0d9c5f877a8fe804317dee1
LaibaAh <la06855@st.habib.edu.pk>
2023-07-16 11:42:09 +0500

    second attempt

--- calculates_results_stats.py
+++ calculates_results_stats.py
@@ -43,57 +43,54 @@
 #       with this function
 # 
 def calculates_results_stats(results_dic):
-    # Creates empty dictionary for results_stats_dic
     results_stats_dic = dict()
+
     results_stats_dic['n_dogs_img'] = 0
     results_stats_dic['n_match'] = 0
     results_stats_dic['n_correct_dogs'] = 0
     results_stats_dic['n_correct_notdogs'] = 0
-    results_stats_dic['n_correct_breed'] = 0       
+    results_stats_dic['n_correct_breed'] = 0
     results_stats_dic['n_images'] = len(results_dic)
- 
+   
     for key in results_dic:
-         
-        # Labels Match Exactly
         if results_dic[key][2] == 1:
             results_stats_dic['n_match'] += 1
-           
-        # Pet Image Label is a Dog - counts number of dog images
+
+        # 5a. Pet Image Label is a Dog AND Labels match - count Correct Breed
+        if results_dic[key][3] == 1 and results_dic[key][4] == 1:
+            results_stats_dic['n_correct_breed'] += 1
+
         if results_dic[key][3] == 1:
             results_stats_dic['n_dogs_img'] += 1
-            
-       # Classifier classifies image as Dog (& pet image is a dog)
-            # counts number of correct dog classifications
-        if results_dic[key][3] == 1 and results_dic[key][4] == 1:
-            results_stats_dic['n_correct_dogs'] += 1
-            
-        if results_dic[key][3] == 0 and results_dic[key][4] == 0:
-            results_stats_dic['n_correct_notdogs'] += 1
-            
-        if results_dic[key][3] == 1 and results_dic[key][2] == 1:
-            results_stats_dic['n_correct_breed'] += 1
-            
-        # calculates number of not-a-dog images using - images & dog images counts
-    results_stats_dic['n_notdogs_img'] = (results_stats_dic['n_images'] - 
-                                      results_stats_dic['n_dogs_img']) 
-        #percentage of correct matches 
-    results_stats_dic['pct_match'] = (results_stats_dic['n_match']/results_stats_dic['n_images'])*100.0
-        
-         # Calculates % correct dogs
-    results_stats_dic['pct_correct_dogs'] = (results_stats_dic['n_correct_dogs']/results_stats_dic['n_dogs_img'])*100.0
-        
-        # Calculates % correct breed of dog
-    results_stats_dic['pct_correct_breed'] = (results_stats_dic['n_correct_breed']/results_stats_dic['n_dogs_img'])*100.0
-        
+            if results_dic[key][4] == 1:
+                results_stats_dic['n_correct_dogs'] += 1
+
+        # 5b. Pet Image Label is NOT a Dog - count number of correct NOT dog classifications
+        else:
+            if results_dic[key][4] == 0:
+                results_stats_dic['n_correct_notdogs'] += 1
+
+   
+    results_stats_dic['n_notdogs_img'] = (results_stats_dic['n_images'] - results_stats_dic['n_dogs_img'])
+
+    # 5c. Calculates % correct for matches
+    results_stats_dic['pct_match'] = (results_stats_dic['n_match'] / results_stats_dic['n_images']) * 100.0
+
+    # 5d. Calculates % correct dogs
+    results_stats_dic['pct_correct_dogs'] = (results_stats_dic['n_correct_dogs'] / results_stats_dic['n_dogs_img']) * 100.0
+
+    # 5e. Calculates % correct breed of dog
+    results_stats_dic['pct_correct_breed'] = (results_stats_dic['n_correct_breed'] / results_stats_dic['n_dogs_img']) * 100.0
+
+    # Calculates % correct not-a-dog images
     if results_stats_dic['n_notdogs_img'] > 0:
-         results_stats_dic['pct_correct_notdogs'] = (results_stats_dic['n_correct_notdogs'] /
-                                                results_stats_dic['n_notdogs_img'])*100.0
+        results_stats_dic['pct_correct_notdogs'] = (results_stats_dic['n_correct_notdogs'] / results_stats_dic['n_notdogs_img']) * 100.0
     else:
-         results_stats_dic['pct_correct_notdogs'] = 0.0
+        results_stats_dic['pct_correct_notdogs'] = 0.0
+
+    # TODO 5f. Return the results_stats_dic dictionary
     return results_stats_dic
-        
-        
-       
+
     
     """
     Calculates statistics of the results of the program run using classifier's model 
